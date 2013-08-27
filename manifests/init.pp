94525544c3ff18fd1754d84f417a3bb2599e5a44
Ryan O'Keeffe <okeefferd@gmail.com>
2013-08-27 12:41:49 -0500

    added tests

--- manifests/init.pp
+++ manifests/init.pp
@@ -38,6 +38,7 @@ class glassfish (
       logoutput   => true,
     }
 
+  }
 }
 
 
---
+++ tests/init.pp
@@ -0,0 +1,11 @@
+class {glassfish:
+  target        => '/u01/glassfish',
+  provider      => 'custom',
+  installfile   => '/tmp/glassfish-3.1.2.2-unix.sh',
+  domain        => 'test_domain1',
+  adminuser     => 'admin',
+  adminpassword => 'helloworld',
+  adminport     => '4848',
+  httpport      => '8080',
+  jdk           => '/u01/java',
+}
